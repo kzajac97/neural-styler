57e10ee93a7570314a164440a0389c7c0cdb5551
krzyzaja <krzysztof.a.zajac@capgemini.com>
2020-12-29 12:36:11 +0100

    Add simple camera activity launch

--- app/src/main/java/com/example/neuralstyler/MainActivity.java
+++ app/src/main/java/com/example/neuralstyler/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.neuralstyler;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+
+@RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity {
+    // UI controls
+    Button takePhotoButton;
+    Button loadPhotoButton;
+    Button stylizePhotoButton;
+    // private fields
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private final String loggerTag = "MainActivityLogger";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // load controls
+        takePhotoButton = findViewById(R.id.takePhotoButton);
+        loadPhotoButton = findViewById(R.id.loadPhotoButton);
+        stylizePhotoButton = findViewById(R.id.stylizePhotoButton);
+        // set listeners
+        takePhotoButton.setOnClickListener(takePhotoButtonOnClickListener);
+        loadPhotoButton.setOnClickListener(loadPhotoButtonOnClickListener);
+        stylizePhotoButton.setOnClickListener(stylizePhotoButtonOnClickListener);
     }
 
     @Override
@@ -37,4 +66,28 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+    final View.OnClickListener takePhotoButtonOnClickListener = v -> {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } catch (ActivityNotFoundException e) {
+                Log.e(loggerTag, "Error! Activity not found!");
+            }
+        }  // permissions granted
+        else {
+            Log.d(loggerTag, "Permission not granted!");
+            requestPermissions(new String[] { Manifest.permission.CAMERA }, 1);
+        }
+    };  // takePhotoButtonOnClickListener
+
+    final View.OnClickListener loadPhotoButtonOnClickListener = v -> {
+        // TODO: Use DB controls to load images from Gallery
+    };  // loadPhotoButtonOnClickListener
+
+    final View.OnClickListener stylizePhotoButtonOnClickListener = v -> {
+        // TODO: Launch GAN activity here
+    };  // stylizePhotoButtonOnClickListener
+
+}  // class
