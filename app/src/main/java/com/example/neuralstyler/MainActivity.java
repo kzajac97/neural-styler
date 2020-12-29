2724954d8c4a3ba75621f44a6cc86a204a6c147d
krzyzaja <krzysztof.a.zajac@capgemini.com>
2020-12-29 11:44:42 +0100

    Remove unnecessary title with UI

--- app/src/main/java/com/example/neuralstyler/MainActivity.java
+++ app/src/main/java/com/example/neuralstyler/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.neuralstyler;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
