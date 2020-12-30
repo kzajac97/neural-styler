9ee4e29964966eb1043f304fea8502ba4b7e2eec
krzyzaja <krzysztof.a.zajac@capgemini.com>
2020-12-30 18:24:18 +0100

    Add setting to NeuralStylerActivity UI

--- app/src/main/java/com/example/neuralstyler/NeuralStylerActivity.java
+++ app/src/main/java/com/example/neuralstyler/NeuralStylerActivity.java
@@ -1,14 +1,47 @@
 package com.example.neuralstyler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NeuralStylerActivity extends AppCompatActivity {
+
+    private final String loggerTag = "NeuralStylerLogger";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neural_styler);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if (item.getItemId() == R.id.action_settings) {
+            Log.d(loggerTag, "Entering settings");
+            Intent enterSettingIntent = new Intent(this, SettingsActivity.class);
+            startActivity(enterSettingIntent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
