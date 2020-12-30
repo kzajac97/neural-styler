4fc006e594d0ee8a9fadfabce2a8f9b767e680db
krzyzaja <krzysztof.a.zajac@capgemini.com>
2020-12-30 17:36:30 +0100

    Add NeuralStylerActivity

---
+++ app/src/main/java/com/example/neuralstyler/NeuralStylerActivity.java
@@ -0,0 +1,14 @@
+package com.example.neuralstyler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NeuralStylerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neural_styler);
+    }
+}
