2e66a17add19bc9fc5b64ec898ba561181177989
krzyzaja <krzysztof.a.zajac@capgemini.com>
2021-01-02 12:49:53 +0100

    Add new activity for adding new styles

---
+++ app/src/main/java/com/example/neuralstyler/StyleManagementActivity.java
@@ -0,0 +1,14 @@
+package com.example.neuralstyler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StyleManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_style_management);
+    }
+}
